b32d6b071342cd02cbf8ab8678a5b8df38141347
habby_chen <habby_c@163.com>
2024-06-14 17:53:27 +0800

    readme

--- src/js/preload.js
+++
@@ -1,6 +0,0 @@
-const {contextBridge, ipcRenderer} = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    getfile: () => ipcRenderer.invoke('dialog:openFile'),
-    downloadFile: (url, filePath) => ipcRenderer.invoke('download-file', { url }),
-});
