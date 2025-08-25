36d871a35486825a163c613ea95b915eed82ad88
habby <habby_c@163.com>
2025-08-25 18:41:06 +0800

    111

--- electron/config.js
+++ electron/config.js
@@ -1,6 +1,7 @@
+const config = {
+  UPDATE_URL: 'https://download.9136347.com/d/%E9%A3%8E%E6%9A%B4%E5%8A%A9%E6%89%8B/update/latest.yml',
+  API_URL: 'https://your-api-server.com',
+  WOW_PATH_CONFIG: './file.json'
+};
 
-const WOW_PATH_CONFIG = './file.json'
-
-module.exports  = {
-    WOW_PATH_CONFIG
-}
+module.exports = config;
